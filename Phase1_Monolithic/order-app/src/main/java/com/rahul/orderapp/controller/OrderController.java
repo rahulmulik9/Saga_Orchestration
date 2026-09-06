e393b9742c27b6599c93c2e2979a97d0af513adf
rahulmulik9 <rahulmulik9@gmail.com>
2026-09-06 18:04:40 +0530

    Added Order controller and service

---
+++ Phase1_Monolithic/order-app/src/main/java/com/rahul/orderapp/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.rahul.orderapp.controller;
+
+import com.rahul.orderapp.entity.Order;
+import com.rahul.orderapp.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order created = orderService.createOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+}
