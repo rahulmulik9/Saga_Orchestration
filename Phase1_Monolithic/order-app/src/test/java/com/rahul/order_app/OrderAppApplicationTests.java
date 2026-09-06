e54bfe6f74e96977673fd6405ad21fa573401f8e
rahulmulik9 <rahulmulik9@gmail.com>
2026-09-06 12:04:29 +0530

    Initial Commit

---
+++ Phase1_Monolithic/order-app/src/test/java/com/rahul/order_app/OrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.order_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
