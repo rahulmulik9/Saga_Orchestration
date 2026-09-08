edb9cd33fa03adccb4b2fb62976719642bac4589
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-08 12:51:47 +0530

    Added new folder for phase 3

---
+++ Phase3_Saga_Orchestration/orderservice/src/main/java/com/rahul/orderservice/client/dto/PaymentRequest.java
@@ -0,0 +1,17 @@
+package com.rahul.orderservice.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    private Long orderId;
+    private BigDecimal amount;
+}
