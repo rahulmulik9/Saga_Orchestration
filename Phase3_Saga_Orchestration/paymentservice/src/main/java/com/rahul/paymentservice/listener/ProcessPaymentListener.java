ab3f8fe7fbb93670ded35042a65770a12c0a4bcc
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-09 09:14:35 +0530

    Added payment failure or payment successful event

--- Phase3_Saga_Orchestration/paymentservice/src/main/java/com/rahul/paymentservice/listener/ProcessPaymentListener.java
+++ Phase3_Saga_Orchestration/paymentservice/src/main/java/com/rahul/paymentservice/listener/ProcessPaymentListener.java
@@ -1,19 +1,41 @@
 package com.rahul.paymentservice.listener;
 
 import com.rahul.paymentservice.dto.sagaDto.KafkaTopics;
+import com.rahul.paymentservice.dto.sagaDto.PaymentCompleted;
+import com.rahul.paymentservice.dto.sagaDto.PaymentFailed;
 import com.rahul.paymentservice.dto.sagaDto.ProcessPaymentCommand;
+import com.rahul.paymentservice.entity.Payment;
+import com.rahul.paymentservice.entity.PaymentStatus;
+import com.rahul.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class ProcessPaymentListener {
+
+    private final PaymentService paymentService;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @KafkaListener(topics = KafkaTopics.PAYMENT_PROCESS, containerFactory = "processPaymentContainerFactory")
     public void handle(ProcessPaymentCommand command) {
         log.info("Received ProcessPaymentCommand for orderId={}, amount={}",
                 command.getOrderId(), command.getAmount());
-        // charge simulation logic comes in Step 5.2
+
+        Payment payment = paymentService.makePayment(command.getOrderId(), command.getAmount());
+
+        log.info("Payment processed for orderId={} -> status={}",
+                command.getOrderId(), payment.getStatus());
+
+        if (payment.getStatus() == PaymentStatus.SUCCESS) {
+            kafkaTemplate.send(KafkaTopics.PAYMENT_COMPLETED, new PaymentCompleted(command.getOrderId()));
+        } else {
+            kafkaTemplate.send(KafkaTopics.PAYMENT_FAILED,
+                    new PaymentFailed(command.getOrderId(), "Payment declined (amount over threshold)"));
+        }
     }
 }
