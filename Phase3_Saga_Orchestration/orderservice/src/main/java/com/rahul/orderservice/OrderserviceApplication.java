329b896722ccf39efc1c3140c6300a17cda39ae8
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-09 11:43:29 +0530

    Small corrections

--- Phase3_Saga_Orchestration/orderservice/src/main/java/com/rahul/orderservice/OrderserviceApplication.java
+++ Phase3_Saga_Orchestration/orderservice/src/main/java/com/rahul/orderservice/OrderserviceApplication.java
@@ -2,7 +2,6 @@ package com.rahul.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class OrderserviceApplication {
--- Phase3_Saga_Orchestration/sagaorchestratorservice/src/main/java/com/rahul/sagaorchestratorservice/entity/SagaState.java
+++ Phase3_Saga_Orchestration/sagaorchestratorservice/src/main/java/com/rahul/sagaorchestratorservice/entity/SagaState.java
@@ -28,6 +28,5 @@ public class SagaState {
 
     private LocalDateTime updatedAt;
 
-    @Lob
     private String itemsJson;
 }
